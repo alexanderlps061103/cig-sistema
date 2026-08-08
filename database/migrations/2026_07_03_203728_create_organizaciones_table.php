af301a56fc64908120f9ba4d9e76f9731181c23e
alexander perez <alexanderlps2013@gmail.com>
2026-08-07 22:48:49 -0400

    Auto-commit: database/migrations/2026_08_08_024048_create_organizaciones_table.php -> database/migrations/2026_07_03_203728_create_organizaciones_table.php at 7/8/2026, 22:48:48

--- database/migrations/2026_07_03_203728_create_organizaciones_table.php
+++ database/migrations/2026_07_03_203728_create_organizaciones_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('organizaciones', function (Blueprint $table) {
             $table->id();
+            $table->string('rif')->unique();
+            $table->string('nombre_razon_social');
+            $table->string('direccion');
+            $table->string('telefono_contacto')->nullable();
+            $table->string('email_contacto')->nullable();
+            $table->string('persona_contacto'); // Tutor Empresarial inicial
+            $table->boolean('estado')->default(true);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('organizaciones');
