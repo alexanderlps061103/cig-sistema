8d0c10f727f1323d5134ca3076586de9f849c8c0
alexander perez <alexanderlps2013@gmail.com>
2026-08-18 22:21:21 -0400

    Add model properties and relationships to ControlDocumentoFisico class

--- app/Models/ControlDocumentoFisico.php
+++ app/Models/ControlDocumentoFisico.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ControlDocumentoFisico extends Model
 {
-    //
+    protected $table = 'control_documentos_fisicos';
+    protected $fillable = [
+        'estudiante_id',
+        'carta_aceptacion_empresa',
+        'evaluacion_tutor_empresarial',
+        'evaluacion_tutor_institucional',
+        'carta_culminacion_empresa',
+        'verificado_por'
+    ];
+
+    public function estudiante() { return $this->belongsTo(Estudiante::class); }
+    public function verificador() { return $this->belongsTo(Persona::class, 'verificado_por'); }
 }
