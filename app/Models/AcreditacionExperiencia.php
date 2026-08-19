0cbad8f7346379a838791d0413587c8c055bb4ea
alexander perez <alexanderlps2013@gmail.com>
2026-08-18 22:29:12 -0400

    Auto-commit: app/Models/AcreditacionExperiencia.php, app/Models/Organizacion.php at 18/8/2026, 22:29:12

--- app/Models/AcreditacionExperiencia.php
+++ app/Models/AcreditacionExperiencia.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class AcreditacionExperiencia extends Model
 {
-    //
+    protected $table = 'acreditaciones_experiencia';
+
+    protected $fillable = [
+        'estudiante_id',
+        'motivos_solicitud',
+        'archivo_formato_1',
+        'archivo_evidencias_digitales',
+        'estado_proceso'
+    ];
+
+    /**
+     * La acreditación pertenece a un estudiante específico.
+     */
+    public function estudiante()
+    {
+        return $this->belongsTo(Estudiante::class, 'estudiante_id');
+    }
 }
--- app/Models/Organizacion.php
+++ app/Models/Organizacion.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Organizacion extends Model
 {
-    //
+    // Nombre de la tabla en plural como definimos en la migración
+    protected $table = 'organizaciones';
+
+    protected $fillable = [
+        'rif',
+        'nombre_razon_social',
+        'direccion',
+        'telefono_contacto',
+        'email_contacto',
+        'persona_contacto',
+        'estado'
+    ];
+
+    /**
+     * Una organización puede tener muchos estudiantes haciendo pasantías.
+     */
+    public function estudiantes()
+    {
+        return $this->hasMany(Estudiante::class, 'organizacion_id');
+    }
 }
